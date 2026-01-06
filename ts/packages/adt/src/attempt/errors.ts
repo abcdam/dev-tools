f0ea48ecabb9e24e6f744d2d56bc503856aa3667
abcdam <gh@abcdam.ch>
2026-01-06 13:28:08 +0100

    [adt]: init "attempt" module

---
+++ ts/packages/adt/src/attempt/errors.ts
@@ -0,0 +1,35 @@
+export interface ISafeExecutionError extends Error {
+  name: "SafeExecutionError";
+  readonly kind: "SafeExecution";
+  readonly originalError: unknown;
+  readonly mapperError: unknown;
+}
+
+export class SafeExecutionError extends Error implements ISafeExecutionError {
+  public override readonly name = "SafeExecutionError"; // Explicit public property
+  public readonly kind = "SafeExecution";
+  public readonly originalError: unknown;
+  public readonly mapperError: unknown;
+  constructor(originalError: unknown, mapperError: unknown) {
+    super(
+      "Critical: The error mapper function crashed. See 'mapperError' and 'originalError'.",
+    );
+    this.originalError = originalError;
+    this.mapperError = mapperError;
+    Object.setPrototypeOf(this, SafeExecutionError.prototype);
+    if (
+      "captureStackTrace" in Error &&
+      typeof Error.captureStackTrace === "function"
+    )
+      Error.captureStackTrace(this, SafeExecutionError);
+  }
+}
+
+export const isSafeExecutionError = (e: unknown): e is SafeExecutionError =>
+  e instanceof SafeExecutionError ||
+  (typeof e === "object" &&
+    !!e &&
+    "kind" in e &&
+    e.kind === "SafeExecution" &&
+    "name" in e &&
+    e.name === "SafeExecutionError");
